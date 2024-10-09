ff3009f369399e6a4ae272caf438ca310e5e9b9a
Jovial-10 <jbrana@cps.edu>
2024-10-09 11:40:52 -0500

    search pa list mr berg

--- a3.py
+++ a3.py
@@ -281,14 +281,17 @@ def search_pa_list(src: List[str]) -> List[str]:
     """
    
     for pattern, action in pa_list:
-        # print(pat, src)
-        val = match(pattern, src)
-        # print(val)
-        if val != None:
-            # print(act)
-            result = action(val)
+        # print(pattern , src, action)
+        mat = match(pattern, src)
+        # print(mat)
+        if mat != None:
+            # print("FOUND")
+            result = action(mat)
             # print(result)
             return result
+
+    return ["I don't understand"]
+
 
 
 def query_loop() -> None:
@@ -313,7 +316,7 @@ def query_loop() -> None:
 # uncomment the following line once you've written all of your code and are ready to try
 # it out. Before running the following line, you should make sure that your code passes
 # the existing asserts.
-# query_loop()
+query_loop()
 
 if __name__ == "__main__":
     assert isinstance(title_by_year(["1974"]), list), "title_by_year not returning a list"
